fc650782217c1f1fb57d9b348d957070a5788c99
Adrenaline-nine <109799192+Adrenaline-nine@users.noreply.github.com>
2022-07-22 20:07:19 +0100

    Update SocialIcons.js

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -12,11 +12,35 @@ const SocialStyles = styled.div`
 export default function SocialIcons({
   icon = <FaBeer />,
   text = 'I need text ',
+  links = [
+    {
+      type: 'Link',
+      title: 'Home',
+      path: '/home',
+    },
+    {
+      type: 'Link',
+      title: 'About',
+      path: '/about',
+    },
+  ],
 }) {
   return (
   <SocialStyles>
     <div className="icon">{icon}</div>
-    <div className="info">        <PText>{text}</PText>
+    <div className="info">  
+    {links.map((item, index) => (
+          <p key={index}>
+            {item.type === 'Link' ? (
+              <Link to={item.path}>{item.title}</Link>
+            ) : (
+              <a href={item.path} target="_blank" rel="noreferrer">
+                {item.title}
+              </a>
+            )}
+          </p>
+        ))}      
+    <PText>{text}</PText>
     </div>
 
   </SocialStyles>
